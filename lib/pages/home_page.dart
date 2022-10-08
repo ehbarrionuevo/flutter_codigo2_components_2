e766d3eba6367333588d45a7a59fbf0e316df648
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-08 11:29:51 -0500

    Item 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -204,6 +204,41 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(24.0)
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 14.0, vertical: 12.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18.0),
+                        color: Colors.amber,
+                      ),
+                      child: Icon(
+                        Icons.arrow_upward,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Sent",
+                        ),
+                        Text(
+                          "Sending Payment to Clients",
+                        )
+                      ],
+                    ),
+                    Text(
+                      "\$150",
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
