d163cebf7fbc279bc0f2a62e16d793a45fe183f6
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-09 09:28:22 -0500

    Modal 1

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -4,32 +4,64 @@ class AlertPage extends StatelessWidget {
   void showAlert1(BuildContext context) {
     showDialog(
       context: context,
+      barrierColor: Colors.black.withOpacity(0.4),
+      barrierDismissible: true,
       builder: (BuildContext context) {
         return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
           contentPadding: EdgeInsets.zero,
-          content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Blog post published",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0
+          content: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/figma.png',
+                  height: 70.0,
                 ),
-              ),
-              const SizedBox(
-                height: 6.0,
-              ),
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-                style: TextStyle(
+                const Text(
+                  "Blog post published",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0),
+                ),
+                const SizedBox(
+                  height: 6.0,
+                ),
+                const Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+                  style: TextStyle(
                     fontWeight: FontWeight.normal,
                     color: Colors.black54,
-                    fontSize: 12.0
+                    fontSize: 12.0,
+                  ),
                 ),
-              ),
-            ],
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Cancelar",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Confirm",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       },
